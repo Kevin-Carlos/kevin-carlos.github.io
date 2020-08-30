a383b27caf80b31fcc157a7b140f2e0200cca924
Kevin Carlos <kcarlos@cycle.io>
2020-08-30 15:46:10 -0700

    remove unused code

--- src/pages/_home/_about-me/_bars.tsx
+++ src/pages/_home/_about-me/_bars.tsx
@@ -11,17 +11,9 @@ interface BarsProps {
 export const Bars: FC<BarsProps> = ({
   className
 }) => {
-  // TODO For now I am explicitly setting these during experimentation
-  // change to a useSprings or useChain implementation
-  // This will clean up a lot of this component
   const [{ y1 }, set1] = useSpring<{ y1: number }>(() => ({ y1: 0 }));
-  // const [{ dash1 }, setDash1] = useSpring<{ dash1: number }>(() => ({ dash1: 300 }));
-
   const [{ y2 }, set2] = useSpring<{ y2: number }>(() => ({ y2: 0 }));
-  // const [{ dash2 }, setDash2] = useSpring<{ dash2: number }>(() => ({ dash2: 200 }));
-
   const [{ y3 }, set3] = useSpring<{ y3: number }>(() => ({ y3: 0 }));
-  // const [{ dash3 }, setDash3] = useSpring<{ dash3: number }>(() => ({ dash3: 200 }));
 
   const [blurb, setBlurb] = useState<BlurbType>("about");
   const [hovered, setHovered] = useState<{ id: BlurbType | null, active: boolean }>({
@@ -66,70 +58,46 @@ export const Bars: FC<BarsProps> = ({
           <Bar1
             onMouseEnter={() => {
               set1({ y1: 5 });
-              // setDash1({ dash1: 0 });
               onMouseEnterHandler("career");
             }}
             onMouseLeave={() => {
               set1({ y1: 0 });
-              // setDash1({ dash1: 300 });
               onMouseLeaveHandler();
             }}
             style={{ transform: y1.interpolate(v => `translateY(${v}%`) }}
           >
             <VerticalWord>
               Career
-              {/* <ProgressLine>
-                <Line
-                  points="3 190, 3 200, 600 200, 600 150, 600 150, 200 150"
-                  style={{ strokeDashoffset: dash1.interpolate(v => `${v}rem`) }}
-                />
-              </ProgressLine> */}
             </VerticalWord>
           </Bar1>
           <Bar2
             onMouseEnter={() => {
               set2({ y2: 5 });
-              // setDash2({ dash2: 0 });
               onMouseEnterHandler("education");
             }}
             onMouseLeave={() => {
               set2({ y2: 0 });
-              // setDash2({ dash2: 200 });
               onMouseLeaveHandler();
             }}
             style={{ transform: y2.interpolate(v => `translateY(${v}%`) }}
           >
             <VerticalWord>
               Education
-              {/* <ProgressLine>
-                <Line
-                  points="3 190, 3 200, 500 200, 500 150, 900 150, 200 150"
-                  // style={{ strokeDashoffset: dash2.interpolate(v => `${v}rem`) }}
-                />
-              </ProgressLine> */}
             </VerticalWord>
           </Bar2>
           <Bar3
             onMouseEnter={() => {
               set3({ y3: 5 });
-              // setDash3({ dash3: 0 });
               onMouseEnterHandler("about");
             }}
             onMouseLeave={() => {
               set3({ y3: 0 });
-              // setDash3({ dash3: 200 });
               onMouseLeaveHandler();
             }}
             style={{ transform: y3.interpolate(v => `translateY(${v}%`) }}
           >
             <VerticalWord>
               About
-              {/* <ProgressLine>
-                <Line
-                  points="3 190, 3 200, 500 200, 500 150, 900 150, 200 150"
-                  // style={{ strokeDashoffset: dash3.interpolate(v => `${v}rem`) }}
-                />
-              </ProgressLine> */}
             </VerticalWord>
           </Bar3>
         </BarWrapper>
@@ -167,15 +135,6 @@ const BarWrapper = styled.div`
   };
 `;
 
-// const ProgressLine = styled.svg`
-//   position: absolute;
-//   bottom: -16rem;
-//   left: 1.2rem;
-//   width: 80rem;
-//   height: 35rem;
-//   pointer-events: none;
-// `;
-
 const Line = styled(animated.polyline)`
   stroke-width: 3px;
   stroke: ${({ theme }) => theme.colors.light_Teal};
@@ -198,27 +157,9 @@ const Bar = styled(animated.div)`
   };
 `;
 
-const Bar1 = styled(Bar)`
-  /* &:hover {
-    ${Line} {
-      animation-play-state: running;
-    }
-  } */
-`;
-const Bar2 = styled(Bar)`
-  &:hover {
-    ${Line} {
-      animation-play-state: running;
-    }
-  }
-`;
-const Bar3 = styled(Bar)`
-  &:hover {
-    ${Line} {
-      animation-play-state: running;
-    }
-  }
-`;
+const Bar1 = styled(Bar)``;
+const Bar2 = styled(Bar)``;
+const Bar3 = styled(Bar)``;
 
 const VerticalWord = styled.span`
   writing-mode: vertical-rl;
