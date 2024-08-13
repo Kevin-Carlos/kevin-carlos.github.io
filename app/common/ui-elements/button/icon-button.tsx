8f28f74f64994cf43a1de0b19ad60dab08ae6a73
Kevin Carlos <kevin_carlos@nevada.unr.edu>
2024-08-12 21:12:34 -0700

    orbit dark/light mode in and out

--- app/common/ui-elements/button/icon-button.tsx
+++ app/common/ui-elements/button/icon-button.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react';
+import type { CSSProperties, ReactElement } from 'react';
 import { Button } from 'react-aria-components';
 import { twMerge } from 'tailwind-merge';
 
@@ -6,20 +6,23 @@ type IconButtonProps = {
   children: ReactElement;
   disabled?: boolean;
   className?: string;
+  style?: CSSProperties;
   onClick: () => void;
 };
 
 export const IconButton = (
-  { children, disabled, className, onClick }: IconButtonProps,
+  { children, disabled, className, style, onClick, ...rest }: IconButtonProps,
 ): ReactElement => {
   return (
     <Button
+      {...rest}
       isDisabled={disabled}
       className={twMerge(
         'focus:outline-theme-orange focus:outline-2 focus:outline rounded-md hover:opacity-60 transition-opacity disabled:opacity-40 disabled:cursor-wait',
         className,
       )}
       onPress={onClick}
+      style={style}
     >
       {children}
     </Button>
