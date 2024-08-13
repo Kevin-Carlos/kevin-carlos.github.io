17bfefdc9cd764f671045ae1ee176d8cca8313fa
Kevin Carlos <kevin_carlos@nevada.unr.edu>
2024-08-12 21:12:34 -0700

    orbit dark/light mode in and out

--- app/common/layout/header/header.tsx
+++ app/common/layout/header/header.tsx
@@ -1,11 +1,13 @@
 import { useFetcher } from '@remix-run/react';
 import { Moon, Sun } from 'phosphor-react';
 import { Fragment, useCallback, useRef, useState } from 'react';
+import { twMerge } from 'tailwind-merge';
 import { useClickOutside } from '~/common/hooks/events';
 import { LinkText } from '~/common/ui-elements';
 import { IconButton } from '~/common/ui-elements/button/icon-button';
 import { useTheme } from '~/useTheme';
 import { menuItems } from '../menu-items';
+import * as styles from './anim.module.css';
 import { HamburgerIcon } from './hamburger-icon';
 import { HamburgerMenu } from './hamburger-menu';
 import { Logo } from './logo';
@@ -14,12 +16,11 @@ const ICON_SIZE = 24;
 
 export const Header = () => {
   const themeMode = useFetcher({ key: 'theme' });
-
   const [mode, setMode] = useTheme();
-
   const [disabled, setDisabled] = useState(false);
-
   const [mobileNav, toggleMobileNav] = useState(false);
+
+  const [interacted, setInteracted] = useState(false);
 
   const menuRef = useRef<HTMLDivElement | null>(null);
   useClickOutside(menuRef, () => toggleMobileNav(false));
@@ -31,6 +32,7 @@ export const Header = () => {
   const onThemeToggle = (m: 'light' | 'dark') => {
     setMode(m);
     setDisabled(true);
+    setInteracted(true);
 
     themeMode.submit(
       { mode: m, url: window.location.pathname },
@@ -42,35 +44,72 @@ export const Header = () => {
 
     setTimeout(() => {
       setDisabled(false);
-    }, 500);
+    }, 1_000);
   };
 
   return (
     <header className='flex py-2 px-6 z-40 h-[56px] text-theme-black dark:text-theme-white sticky top-0 bg-theme-white dark:bg-theme-black border-b-2 border-b-theme-dteal md:border-b-0'>
       <div className='flex gap-[16px] justify-center items-center'>
-        <Logo />
-
-        <IconButton
-          disabled={disabled}
-          onClick={() => {
-            mode === 'light' ? onThemeToggle('dark') : onThemeToggle('light');
-          }}
-          className='bg-theme-lgray dark:bg-theme-dteal rounded-full h-8 w-8 flex justify-center items-center z-10 hover:opacity-100 disabled:opacity-100 group'
-        >
-          {mode === 'light'
-            ? (
-              <Moon
-                size={ICON_SIZE}
-                className='group-hover:opacity-40 group-disabled:opacity-40'
-              />
-            )
-            : (
-              <Sun
-                size={ICON_SIZE}
-                className='group-hover:opacity-40 group-disabled:opacity-40'
-              />
+        <Logo>
+          <IconButton
+            aria-label='Dark Mode'
+            disabled={disabled}
+            onClick={() => {
+              onThemeToggle('dark');
+            }}
+            className={twMerge(
+              'bg-theme-lgray dark:bg-theme-dteal rounded-full h-8 w-8 flex justify-center items-center z-10 hover:opacity-100 disabled:opacity-100 group absolute',
+              // @ts-expect-error css module typings
+              interacted && mode === 'dark' && styles.orbitOut,
+              // @ts-expect-error css module typings
+              interacted && mode === 'light' && styles.orbitIn,
             )}
-        </IconButton>
+            style={{
+              animationFillMode: 'forwards',
+
+              // For the math behind these values see also anim.module.css
+              // https://www.useragentman.com/blog/2013/03/03/animating-circular-paths-using-css3-transitions/
+              transform: interacted
+                ? undefined
+                : mode === 'light'
+                ? 'rotate(360deg) translate3d(56px, 4px, 0) rotate(-360deg)'
+                : 'rotate(180deg) translate3d(56px, 4px, 0) rotate(-180deg)',
+            }}
+          >
+            <Moon
+              size={ICON_SIZE}
+              className='group-hover:opacity-40 group-hover:scale-95 group-disabled:opacity-40'
+            />
+          </IconButton>
+          <IconButton
+            aria-label='Light Mode'
+            disabled={disabled}
+            onClick={() => {
+              onThemeToggle('light');
+            }}
+            className={twMerge(
+              'bg-theme-lgray dark:bg-theme-dteal rounded-full h-8 w-8 flex justify-center items-center z-10 hover:opacity-100 disabled:opacity-100 group absolute',
+              // @ts-expect-error css module typings
+              interacted && mode === 'light' && styles.orbitOut,
+              // @ts-expect-error css module typings
+              interacted && mode === 'dark' && styles.orbitIn,
+            )}
+            style={{
+              // For the math behind these values see also anim.module.css
+              // https://www.useragentman.com/blog/2013/03/03/animating-circular-paths-using-css3-transitions/
+              transform: interacted
+                ? undefined
+                : mode === 'dark'
+                ? 'rotate(360deg) translate3d(56px, 4px, 0) rotate(-360deg)'
+                : 'rotate(180deg) translate3d(56px, 4px, 0) rotate(-180deg)',
+            }}
+          >
+            <Sun
+              size={ICON_SIZE}
+              className='group-hover:opacity-40 group-hover:scale-95 group-disabled:opacity-40'
+            />
+          </IconButton>
+        </Logo>
       </div>
       <nav className='hidden grow-[1] items-center justify-end sm:flex h-[40px]'>
         {menuItems.map((mi) => {
--- app/common/layout/header/logo.tsx
+++ app/common/layout/header/logo.tsx
@@ -1,18 +1,19 @@
 import { Link } from '@remix-run/react';
 import clsx from 'clsx';
+import { ReactNode } from 'react';
 import darkLogo from '~/common/assets/images/logo-dark.png';
 import { links } from '~/common/links';
 import * as styles from './anim.module.css';
 
-export const Logo = () => {
+export const Logo = ({ children }: { children: ReactNode }) => {
   return (
     <div className='w-[40px] h-[40px] relative'>
       <Link
         aria-label='Home'
         to={links.home()}
-        className='focus:outline-theme-dteal dark:focus:outline-theme-orange rounded-full focus:outline-offset-2 focus:outline focus:outline-2 z-10'
+        className='focus:outline-theme-dteal dark:focus:outline-theme-orange rounded-full focus:outline-offset-2 focus:outline focus:outline-2 z-10 group'
       >
-        <div className='w-[40px] h-[40px] rounded-full border-0 bg-theme-lgray dark:bg-theme-white absolute transition-opacity'>
+        <div className='w-[40px] h-[40px] rounded-full border-0 bg-theme-lgray dark:bg-theme-white absolute transition-transform group-hover:scale-95 group-hover:opacity-95'>
           <img src={darkLogo} style={{ position: 'absolute', width: '40px' }} />
         </div>
       </Link>
@@ -32,12 +33,14 @@ export const Logo = () => {
             className={clsx(
               // @ts-expect-error css module typings
               styles.spinSlow,
-              'border-2 border-theme-dteal dark:border-theme-orange w-[115px] h-[115px] rounded-full absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]',
-              'before:h-[8px] before:w-[8px] before:rounded-full before:bg-theme-black dark:before:bg-theme-lteal2 before:flex before:translate-y-[29px] before:shadow-lg dark:before:shadow-[2px_5px_5px_rgba(0,0,0,0.8)]',
+              'border-2 border-theme-dteal dark:border-theme-orange w-[110px] h-[110px] rounded-full absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]',
+              'before:h-[8px] before:w-[8px] before:rounded-full before:bg-theme-black dark:before:bg-theme-lteal2 before:flex before:translate-y-[27px] before:shadow-lg dark:before:shadow-[2px_5px_5px_rgba(0,0,0,0.8)]',
             )}
           />
         </div>
       </div>
+
+      {children}
     </div>
   );
 };
