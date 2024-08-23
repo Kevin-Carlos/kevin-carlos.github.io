91fae2022565924c634c7419fbb63152dd17713b
Kevin Carlos <kevin_carlos@nevada.unr.edu>
2024-08-22 17:02:45 -0700

    nuxt like cards

--- app/routes/projects/project-items.tsx
+++ app/routes/projects/project-items.tsx
@@ -1,29 +1,30 @@
-import { ReactNode } from 'react';
 import { links } from '~/common/links';
+import type { CardProps } from '~/common/ui-elements/cards/card';
 
-type ProjectItem = {
-  name: string;
-  description: ReactNode;
-  path: string;
-  linkText: string;
-};
+type ProjectItem = CardProps;
 
 export const projectItems: ProjectItem[] = [
   {
-    name: 'Loot Crates',
-    description: (
+    title: 'Loot Crates',
+    children: (
       <p>
         An interactive loot crate system built to learn and understand the
         inner-workings of loot boxes.
       </p>
     ),
-    path: links.projects().project().lootCrate(),
+    to: links.projects().project().lootCrate(),
     linkText: 'Loot Crate Demo',
   },
   {
-    name: 'Loaders',
-    description: <p>Building loading spinners / indicators for fun.</p>,
-    path: links.projects().project().loaders(),
+    title: 'Loaders',
+    children: <p>Building loading spinners / indicators for fun.</p>,
+    to: links.projects().project().loaders(),
     linkText: 'Loading Spinner Demo',
+  },
+  {
+    title: 'Toast Center',
+    children: <p>A take on a notification center via toasts</p>,
+    to: links.projects().project().snackCenter(),
+    linkText: 'Toast Center Demo',
   },
 ];
