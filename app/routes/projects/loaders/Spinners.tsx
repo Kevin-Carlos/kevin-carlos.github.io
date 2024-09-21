a287b0c92b68c89deb612db2e289267bd2b17dca
Kevin Carlos <kevin_carlos@nevada.unr.edu>
2024-09-20 17:21:02 -0700

    zelda loading spinner

--- app/routes/projects/loaders/Spinners.tsx
+++ app/routes/projects/loaders/Spinners.tsx
@@ -1,12 +1,69 @@
 import clsx from 'clsx';
 import { useTheme } from '~/useTheme';
 import * as styles from './spinners.module.css';
+import * as triangles from './triangles.module.css';
+
+const TriangleDot = ({
+  className,
+}: { className?: string }) => {
+  return (
+    <div
+      className={clsx(
+        'absolute',
+        'rounded-full',
+        'h-[10px]',
+        'w-[10px]',
+        'bg-theme-black',
+        'dark:bg-theme-white',
+        className,
+      )}
+    />
+  );
+};
 
 export const Spinners = () => {
   const [mode] = useTheme();
 
   return (
     <>
+      <div className='relative h-[60px] w-[60px]'>
+        {/* Top / Triangle 1 */}
+        <TriangleDot
+          // @ts-expect-error css module
+          className={clsx('top-0 left-[50%]', triangles.tip1)}
+        />
+
+        {/* Right / Triangle 2 */}
+        <TriangleDot
+          // @ts-expect-error css module
+          className={clsx('right-0 top-[20%]', triangles.triangle2)}
+        />
+
+        {/* Right / Triangle 1 */}
+        <TriangleDot
+          // @ts-expect-error css module
+          className={clsx('right-0 bottom-[20%]', triangles.triangle1)}
+        />
+
+        {/* Left / Triangle 2 */}
+        <TriangleDot
+          // @ts-expect-error css module
+          className={clsx('left-0 top-[20%]', triangles.triangle2)}
+        />
+
+        {/* Left / Triangle 1 */}
+        <TriangleDot
+          // @ts-expect-error css module
+          className={clsx('left-0 bottom-[20%]', triangles.triangle1)}
+        />
+
+        {/* Bottom / Triangle 2 */}
+        <TriangleDot
+          // @ts-expect-error css module
+          className={clsx('bottom-0 left-[50%]', triangles.tip2)}
+        />
+      </div>
+
       <div
         // @ts-expect-error typing modules
         className={styles['alternate-spinner']}
